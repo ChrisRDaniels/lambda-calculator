7afd0e161ed15bae269075b34470d9a5e287c0b3
Damaged0ne <43980544+Damaged0ne@users.noreply.github.com>
2019-09-11 13:15:20 -0600

    took out unnecessary comments to clean up code

--- src/components/ButtonComponents/OperatorButtons/Operators.js
+++ src/components/ButtonComponents/OperatorButtons/Operators.js
@@ -2,12 +2,8 @@ import React, {useState} from "react";
 import { operators } from "../../../data";
 import OperatorButton from "./OperatorButton";
 
-//import any components needed
-
-//Import your array data to from the provided data file
-
 const Operators = () => {
-  // STEP 2 - add the imported data to state
+
   const [operatorsState] = useState(operators);
   return (
     <div className="Operators">
--- src/components/ButtonComponents/SpecialButtons/Specials.js
+++ src/components/ButtonComponents/SpecialButtons/Specials.js
@@ -2,12 +2,8 @@ import React, {useState} from "react";
 import { specials } from "../../../data";
 import SpecialButton from "./SpecialButton";
 
-//import any components needed
-
-//Import your array data to from the provided data file
-
 const Specials = () => {
-  // STEP 2 - add the imported data to state
+
   const [specialsState] = useState(specials);
   return (
     <div className="Specials">
