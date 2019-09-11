4ee97aba6c1e04f3dedf50093c6d3df45a7d603f
Damaged0ne <43980544+Damaged0ne@users.noreply.github.com>
2019-09-11 10:06:30 -0600

    working mvp

--- src/components/DisplayComponents/Display.js
+++ src/components/DisplayComponents/Display.js
@@ -1,5 +1,10 @@
-import React from "react";
+import React, {useState} from "react";
 
 const Display = () => {
-  return <div className="display">{/* Display any props data here */}</div>;
+  const [math, calcMath] = useState(12345);
+
+  return <div className="display">
+  {math}</div>;
 };
+
+export default Display;
